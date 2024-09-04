39cd6c679a0c1d8f939136ed38de4148cb083616
Carlos Mundaray <carlosmundaray777@gmail.com>
2024-09-04 11:20:38 -0400

    Delete task

--- client/src/api/tasks.js
+++ client/src/api/tasks.js
@@ -9,4 +9,4 @@ export const createTaskRequest=(task)=>axios.post('/tasks',task);
 export const updateTaskRequest=(id,task)=>
 axios.put(`/tasks/${id}`,task);
 
-export const deleteTaskRequest=(id)=>axios.get(`/tasks/${id}`);
+export const deleteTaskRequest=(id)=>axios.delete(`/tasks/${id}`);
